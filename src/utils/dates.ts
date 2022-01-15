7b38287f34e06c641427aabc9003c2c7d9a869c4
benyakir <benyakir.horowitz@gmail.com>
2022-01-15 19:37:50 +0100

    fix switch statement for golang

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,105 +1,105 @@
 export function getTimeFromDateString(date: string): DateInformation {
-    const month = +date.substring(0, 2);
-    const year = +date.substring(6)
-    if (isNaN(month) || month > 12 || isNaN(year)) {
-        throw new Error(`Unable to parse date from string ${date}`)
-    }
-    return {
-        year,
-        month,
-        ...getMonth(month),
-        date: new Date(date)
-    }
+  const month = +date.substring(0, 2);
+  const year = +date.substring(6)
+  if (isNaN(month) || month > 12 || isNaN(year)) {
+    throw new Error(`Unable to parse date from string ${date}`)
+  }
+  return {
+    year,
+    month,
+    ...getMonth(month),
+    date: new Date(date)
+  }
 }
 
 export function getBlogPostDateInformation(date: string): DateInformation {
-    const year = +date.substring(0, 4)
-    const month = +date.substring(5, 7)
-    if (isNaN(year) || isNaN(month) || month > 12) {
-        throw new Error('Unable to parse date')
-    }
-    return {
-        year,
-        month,
-        ...getMonth(month),
-        date: new Date(date)
-    }
+  const year = +date.substring(0, 4)
+  const month = +date.substring(5, 7)
+  if (isNaN(year) || isNaN(month) || month > 12) {
+    throw new Error('Unable to parse date')
+  }
+  return {
+    year,
+    month,
+    ...getMonth(month),
+    date: new Date(date)
+  }
 }
 
 export const getPrettyDate = (date: Date) => (
-    date.toLocaleString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "2-digit",
-    })
+  date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  })
 )
 
 export function getMonth(month: number): FullMonth {
-    switch (month) {
-        case 1:
-            return {
-                full: "January",
-                short: "JAN"
-            }
-        case 2:
-            return {
-                full: "February",
-                short: "FEB"
-            }
-        case 3:
-            return {
-                full: "March",
-                short: "MAR"
-            }
-        case 4:
-            return {
-                full: "April",
-                short: "APR"
-            }
-        case 5:
-            return {
-                full: "May",
-                short: "MAY"
-            }
-        case 6:
-            return {
-                full: "June",
-                short: "JUN"
-            }
-        case 7:
-            return {
-                full: "July",
-                short: "JUL"
-            }
-        case 8:
-            return {
-                full: "August",
-                short: "AUG"
-            }
-        case 9:
-            return {
-                full: "September",
-                short: "SEP"
-            }
-        case 10:
-            return {
-                full: "October",
-                short: "OCT"
-            }
-        case 11:
-            return {
-                full: "November",
-                short: "NOV"
-            }
-        case 12:
-            return {
-                full: "December",
-                short: "DEC"
-            }
-        default:
-            return {
-                full: "January",
-                short: "JAN"
-            }
-    }
+  switch (month) {
+    case 1:
+      return {
+        full: "January",
+        short: "JAN"
+      }
+    case 2:
+      return {
+        full: "February",
+        short: "FEB"
+      }
+    case 3:
+      return {
+        full: "March",
+        short: "MAR"
+      }
+    case 4:
+      return {
+        full: "April",
+        short: "APR"
+      }
+    case 5:
+      return {
+        full: "May",
+        short: "MAY"
+      }
+    case 6:
+      return {
+        full: "June",
+        short: "JUN"
+      }
+    case 7:
+      return {
+        full: "July",
+        short: "JUL"
+      }
+    case 8:
+      return {
+        full: "August",
+        short: "AUG"
+      }
+    case 9:
+      return {
+        full: "September",
+        short: "SEP"
+      }
+    case 10:
+      return {
+        full: "October",
+        short: "OCT"
+      }
+    case 11:
+      return {
+        full: "November",
+        short: "NOV"
+      }
+    case 12:
+      return {
+        full: "December",
+        short: "DEC"
+      }
+    default:
+      return {
+        full: "January",
+        short: "JAN"
+      }
+  }
 }
--- src/utils/project.ts
+++ src/utils/project.ts
@@ -4,90 +4,92 @@ import { createSearchableString } from './posts';
 import { FlattenedProject, PartialFlattenedProject, ProjectType } from "@Types/posts";
 
 export function formatAllProjects(projects: ProjectType[]): FlattenedProject[] {
-    return projects.map(p => formatProject(p)).sort((a, b) => b.firstReleased.date.getTime() - a.firstReleased.date.getTime() );
+  return projects.map(p => formatProject(p)).sort((a, b) => b.firstReleased.date.getTime() - a.firstReleased.date.getTime());
 }
 
 export function formatProject(project: ProjectType): FlattenedProject {
-    const tech = project.project.technologies.split(", ");
-    const piecemealProject: PartialFlattenedProject = {
-        title: project.title,
-        slug: project.slug,
-        mainLink: project.project.mainLink,
-        hostedOn: project.project.hostedOn,
-        repoLink: project.project.repoLink,
-        content: project.content,
-        firstReleased: getTimeFromDateString(project.project.firstReleased),
-        shortTechnologies: tech,
-        longTechnologies: tech.map(t => getFullTechName(t)),
-    }
-    if (project.project.latestUpdate) {
-        piecemealProject.latestUpdate = getTimeFromDateString(project.project.latestUpdate)
-    }
-    const _project: FlattenedProject = {
-        ...piecemealProject,
-        meta: createMetaForProject(piecemealProject)
-    }
-    return _project;
+  const tech = project.project.technologies.split(", ");
+  const piecemealProject: PartialFlattenedProject = {
+    title: project.title,
+    slug: project.slug,
+    mainLink: project.project.mainLink,
+    hostedOn: project.project.hostedOn,
+    repoLink: project.project.repoLink,
+    content: project.content,
+    firstReleased: getTimeFromDateString(project.project.firstReleased),
+    shortTechnologies: tech,
+    longTechnologies: tech.map(t => getFullTechName(t)),
+  }
+  if (project.project.latestUpdate) {
+    piecemealProject.latestUpdate = getTimeFromDateString(project.project.latestUpdate)
+  }
+  const _project: FlattenedProject = {
+    ...piecemealProject,
+    meta: createMetaForProject(piecemealProject)
+  }
+  return _project;
 }
 
 export function createMetaForProject(project: PartialFlattenedProject) {
-    const data = [
-        project.title,
-        project.slug,
-        project.content,
-        project.hostedOn,
-        project.firstReleased.year,
-        project.firstReleased.full,
-        project.firstReleased.short,
-        ...project.shortTechnologies,
-        ...project.longTechnologies
-    ];
-    if (project.repoLink) {
-        data.push('github')
-        data.push('repo')
-    }
-    return createSearchableString(data.filter(d => !!d))
+  const data = [
+    project.title,
+    project.slug,
+    project.content,
+    project.hostedOn,
+    project.firstReleased.year,
+    project.firstReleased.full,
+    project.firstReleased.short,
+    ...project.shortTechnologies,
+    ...project.longTechnologies
+  ];
+  if (project.repoLink) {
+    data.push('github')
+    data.push('repo')
+  }
+  return createSearchableString(data.filter(d => !!d))
 }
 
 export function getFullTechName(tech: string) {
-    switch (tech.toLowerCase()) {
-        case "html":
-            return "HTML";
-        case "css":
-            return "CSS";
-        case "sass":
-            return "Sass";
-        case "js":
-            return "JavaScript";
-        case "ts":
-            return "TypeScript";
-        case "py":
-            return "Python";
-        case "php":
-            return "PHP";
-        case "wp":
-            return "WordPress";
-        case "ng":
-            return "Angular";
-        case "swift":
-            return "Swift";
-        case "react":
-            return "React";
-        case "vue":
-            return "Vue";
-        case "cs":
-            return "C#";
-        case "unity":
-            return "Unity";
-        case "ml":
-            return "Machine Learning";
-        case "svelte":
-            return "Svelte";
-        case "gql":
-            return "GraphQL";
-        case "tw":
-            return "Tailwind";
-        default:
-            return tech;
-    }
+  switch (tech.toLowerCase()) {
+    case "html":
+      return "HTML";
+    case "css":
+      return "CSS";
+    case "sass":
+      return "Sass";
+    case "js":
+      return "JavaScript";
+    case "ts":
+      return "TypeScript";
+    case "py":
+      return "Python";
+    case "php":
+      return "PHP";
+    case "wp":
+      return "WordPress";
+    case "ng":
+      return "Angular";
+    case "swift":
+      return "Swift";
+    case "react":
+      return "React";
+    case "vue":
+      return "Vue";
+    case "cs":
+      return "C#";
+    case "unity":
+      return "Unity";
+    case "ml":
+      return "Machine Learning";
+    case "svelte":
+      return "Svelte";
+    case "gql":
+      return "GraphQL";
+    case "tw":
+      return "Tailwind";
+    case "go":
+      return "Golang";
+    default:
+      return tech;
+  }
 }
