45d105e100a94fa224ea69a84d71c4b42dd3010d
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-07-29 17:34:13 +0200

    Add a simple test for createChoiceSet [ci skip]

--- src/utils/tests/filter.spec.ts
+++ src/utils/tests/filter.spec.ts
@@ -4,8 +4,18 @@ import * as filter from "@Utils/filter";
 
 describe("filter util", () => {
 	describe("createChoiceSet", () => {
-		it("should return a set of all items at that key value that are truthy as an object of the expected dimensions", () => {
-			// TODO
+		it("should return only unique objects", () => {
+			const items = [
+				{ name: ["John"], something: "Value" },
+				{ name: ["Doe"], something: "Value2" },
+				{ name: ["John"], something: "Value3" },
+			];
+			const got = filter.createChoiceSet(items, "name");
+			expect(got).toHaveLength(2);
+			expect(got).toEqual([
+				{ label: "John", value: "John" },
+				{ label: "Doe", value: "Doe" },
+			]);
 		});
 	});
 });
