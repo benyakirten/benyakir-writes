d850d006dff11c21efa5e8f35ab6254650429eec
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-08-08 16:28:43 +0200

    Create a GrowableUnderline wrapper component [ci skip]

---
+++ src/components/General/GrowableUnderline/GrowableUnderline.component.tsx
@@ -0,0 +1,33 @@
+import { media } from "@/styles/queries";
+import React from "react";
+import styled from "styled-components";
+
+const UnderlinableItem = styled.div<{ display?: string }>`
+    display: ${(props) => props.display ?? "inline"};
+
+	color: ${(props) => props.theme.link.dark};
+
+	background-color: transparent;
+	background-image: linear-gradient(${(props) => props.theme.link.dark}, ${(props) => props.theme.link.dark});
+	background-repeat: no-repeat;
+	background-size: 0px 2px;
+	background-position: 0 100%;
+	text-decoration: none;
+
+	transition: background-size 0.3s;
+
+	&:hover {
+		background-size: 100% 2px;
+	}
+
+    ${media.noHover} {
+      background-size: 100% 2px;
+    }
+`;
+
+const GrowableUnderline: React.FC<{ display?: string } & ChildrenProp> = ({
+	children,
+	display,
+}) => <UnderlinableItem display={display}>{children}</UnderlinableItem>;
+
+export default GrowableUnderline;
--- src/components/Layout/NewSearch/Results/EmptyResults.component.tsx
+++ src/components/Layout/NewSearch/Results/EmptyResults.component.tsx
@@ -1,29 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-import { FONT_LG, FONT_MD, FONT_SM, SIZE_SM } from "@/styles/variables";
+import { FONT_LG, FONT_MD, SIZE_SM } from "@/styles/variables";
+import { GrowableUnderline } from "@/components/General";
 
 const EmptyContainer = styled.div`
     padding: ${SIZE_SM};
 	font-size: ${FONT_MD};
 	line-height: ${FONT_LG};
-`;
-
-const PossibleSearch = styled.button`
-	color: ${(props) => props.theme.link.dark};
-
-	background-color: transparent;
-	background-image: linear-gradient(${(props) => props.theme.link.dark}, ${(props) => props.theme.link.dark});
-	background-repeat: no-repeat;
-	background-size: 0px 2px;
-	background-position: 0 100%;
-	text-decoration: none;
-
-	transition: background-size 0.3s;
-
-	&:hover, &:focus {
-		background-size: 100% 2px;
-	}
 `;
 
 const EmptyResults: React.FC<{
@@ -36,9 +20,11 @@ const EmptyResults: React.FC<{
 				No results found. Did you mean:{" "}
 				{alternatives.map((alternative, i) => (
 					<span key={alternative}>
-						<PossibleSearch onClick={() => onSelect(alternative)}>
-							{alternative}
-						</PossibleSearch>
+						<GrowableUnderline>
+							<button type="button" onClick={() => onSelect(alternative)}>
+								{alternative}
+							</button>
+						</GrowableUnderline>
 						<span>{i < alternatives.length - 1 ? ",  " : ""}</span>
 					</span>
 				))}
