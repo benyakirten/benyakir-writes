1f04fe941ab4c5280ba632931d350625d42e9025
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-08-10 19:24:14 +0200

    Use new date function in DatePicker component [ci skip]

--- src/components/Input/DatePicker/DatePicker.component.tsx
+++ src/components/Input/DatePicker/DatePicker.component.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 
 import { DateContainer, DateInput } from "./DatePicker.styles";
-import { convertDatePickerValueToDate } from "@/utils/dates";
+import {
+	convertDatePickerValueToDate,
+	convertDateToDatePickerValue,
+} from "@/utils/dates";
 
 const DatePicker: React.FC<DateProps> = ({
 	name,
@@ -10,13 +13,7 @@ const DatePicker: React.FC<DateProps> = ({
 	value,
 	tabIndex = 0,
 }) => {
-	// I have no idea why getMonth() outputs months as 0-11 when date pickers use 1-12.
-	const month = (value.getMonth() + 1).toString();
-	const date = value.getDate().toString();
-	const convertedDate = `${value.getFullYear()}-${month.padStart(
-		2,
-		"0",
-	)}-${date.padStart(2, "0")}`;
+	const convertedDate = convertDateToDatePickerValue(value);
 	const handleChange = (e: React.BaseSyntheticEvent) => {
 		const date = convertDatePickerValueToDate(e.target.value);
 		onChange(date);
