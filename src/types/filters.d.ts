05df09a731a7cd84e9bd457fe0de93e70d5f99a1
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-10-24 06:28:00 -0600

    Beginw orking on filter tests [ci skip]

--- src/types/filters.d.ts
+++ src/types/filters.d.ts
@@ -1,121 +1,122 @@
 import { useFlyout } from "@/hooks/useFlyout.hook";
+import { PotentialChoice } from "./general";
 
 type FilterOption = {
-	label: string;
-	id: string;
-	disabled: boolean;
+  label: string;
+  id: string;
+  disabled: boolean;
 };
 
 type NewFilterProps = {
-	onCreate: (val: string) => void;
-	options: FilterOption[];
-	menuOpenTop: boolean;
-	menuOpen: boolean;
-	setSoftOpen: React.Dispatch<React.SetStateAction<boolean>>;
-	setHardOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  onCreate: (val: string) => void;
+  options: FilterOption[];
+  menuOpenTop: boolean;
+  menuOpen: boolean;
+  setSoftOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setHardOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 type FilterProps = {
-	onCreate: (id: string) => void;
-	onRemove: (id: string) => void;
-	onModifyDate: (time: "start" | "end", value: Date) => void;
-	onModifyKeywords: (id: string, keywords: readonly PotentialChoice[]) => void;
-	onModifyWordFilterType: (id: string, type: WordFilterType) => void;
-	onModifySearch: (id: string, search: string) => void;
-	options: FilterOption[];
-	filters: ItemFilter[];
-	currentPage: number;
-	numPages: number;
-	setPage: React.Dispatch<React.SetStateAction<number>>;
+  onCreate: (id: string) => void;
+  onRemove: (id: string) => void;
+  onModifyDate: (time: "start" | "end", value: Date) => void;
+  onModifyKeywords: (id: string, keywords: readonly PotentialChoice[]) => void;
+  onModifyWordFilterType: (id: string, type: WordFilterType) => void;
+  onModifySearch: (id: string, search: string) => void;
+  options: FilterOption[];
+  filters: ItemFilter[];
+  currentPage: number;
+  numPages: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
 type MultipleChoiceInputProps = {
-	label: string;
-	choices: PotentialChoice[];
-	onSelect: (choices: readonly PotentialChoice[]) => void;
+  label: string;
+  choices: PotentialChoice[];
+  onSelect: (choices: readonly PotentialChoice[]) => void;
 };
 
 type TextInputProps = {
-	label: string;
-	name: string;
-	onChange: (val: string) => void;
-	value: string;
+  label: string;
+  name: string;
+  onChange: (val: string) => void;
+  value: string;
 };
 
 type BorderRadiusCorners = {
-	topLeft?: string;
-	topRight?: string;
-	bottomLeft?: string;
-	bottomRight?: string;
+  topLeft?: string;
+  topRight?: string;
+  bottomLeft?: string;
+  bottomRight?: string;
 };
 
 type FilterPillProps = ChildrenProp & {
-	onRemove: () => void;
+  onRemove: () => void;
 };
 
 type FilterButtonProps = ChildrenProp &
-	React.ButtonHTMLAttributes<HTMLButtonElement> & {
-		borderRadiusCorners?: BorderRadiusCorners;
-		filledIn?: boolean;
-		width?: string;
-	};
+  React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    borderRadiusCorners?: BorderRadiusCorners;
+    filledIn?: boolean;
+    width?: string;
+  };
 
 type DateFilterProps = DateFilter & {
-	onModify: (time: "start" | "end", value: Date) => void;
-	onRemove: () => void;
+  onModify: (time: "start" | "end", value: Date) => void;
+  onRemove: () => void;
 };
 
 type KeywordFilterProps = KeywordFilter & {
-	onModify: (keywords: readonly PotentialChoice[]) => void;
-	onChangeType: (type: WordFilterType) => void;
-	onRemove: () => void;
+  onModify: (keywords: readonly PotentialChoice[]) => void;
+  onChangeType: (type: WordFilterType) => void;
+  onRemove: () => void;
 };
 
 type SearchFilterProps = SearchFilter & {
-	onSearch: (search: string) => void;
-	onChangeType: (type: WordFilterType) => void;
-	onRemove: () => void;
+  onSearch: (search: string) => void;
+  onChangeType: (type: WordFilterType) => void;
+  onRemove: () => void;
 };
 
 type ItemFilter = DateFilter | KeywordFilter | SearchFilter;
 
 type DateFilter = {
-	id: "date";
-	label: "Date";
-	start: Date;
-	end: Date;
+  id: "date";
+  label: "Date";
+  start: Date;
+  end: Date;
 };
 
 type WordFilterType = "any" | "all";
 type KeywordFilter = {
-	id: string;
-	label: string;
-	type: WordFilterType;
-	currentKeywords: readonly PotentialChoice[];
-	allKeywords: PotentialChoice[];
+  id: string;
+  label: string;
+  type: WordFilterType;
+  currentKeywords: readonly PotentialChoice[];
+  allKeywords: PotentialChoice[];
 };
 
 type SearchFilter = {
-	id: Exclude<string, "date">;
-	label: string;
-	type: WordFilterType;
-	search: string[];
+  id: Exclude<string, "date">;
+  label: string;
+  type: WordFilterType;
+  search: string[];
 };
 
 type CurrentPageProps = {
-	currentPage: number;
-	numPages: number;
-	onSetPage: (page: number) => void;
+  currentPage: number;
+  numPages: number;
+  onSetPage: (page: number) => void;
 };
 
 type CreateFilterOption = {
-	match: string;
-	fn: () => void;
+  match: string;
+  fn: () => void;
 };
 
 type KeywordFilterDetails = {
-	id: string;
-	allKeywords: PotentialChoice[];
+  id: string;
+  allKeywords: PotentialChoice[];
 };
 
 type ParsedQueryParams = Map<string, number | string[]>;
--- src/utils/tests/filter.spec.ts
+++ src/utils/tests/filter.spec.ts
@@ -1,21 +1,100 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 
-import * as filter from "@/utils/filter";
+import {
+  createChoiceSet,
+  isDateFilter,
+  isKeywordFilter,
+  isSearchFilter,
+} from "@/utils/filter";
+import { ItemFilter } from "@/types/filters";
 
-describe("filter util", () => {
-	describe("createChoiceSet", () => {
-		it("should return only unique objects", () => {
-			const items = [
-				{ name: ["John"], something: "Value" },
-				{ name: ["Doe"], something: "Value2" },
-				{ name: ["John"], something: "Value3" },
-			];
-			const got = filter.createChoiceSet(items, "name");
-			expect(got).toHaveLength(2);
-			expect(got).toEqual([
-				{ label: "John", value: "John" },
-				{ label: "Doe", value: "Doe" },
-			]);
-		});
-	});
+beforeEach(() => {
+  window.history.pushState({}, "", "/");
+});
+
+describe("createChoiceSet", () => {
+  it("should return only unique objects", () => {
+    const items = [
+      { name: ["John"], something: "Value" },
+      { name: ["Doe"], something: "Value2" },
+      { name: ["John"], something: "Value3" },
+    ];
+    const got = createChoiceSet(items, "name");
+    expect(got).toHaveLength(2);
+    expect(got).toEqual([
+      { label: "John", value: "John" },
+      { label: "Doe", value: "Doe" },
+    ]);
+  });
+});
+
+describe("createChoiceSet", () => {
+  it("should return only unique objects", () => {
+    const items = [
+      { name: ["John"], something: "Value" },
+      { name: ["Doe"], something: "Value2" },
+      { name: ["John"], something: "Value3" },
+    ];
+    const got = createChoiceSet(items, "name");
+    expect(got).toHaveLength(2);
+    expect(got).toEqual([
+      { label: "John", value: "John" },
+      { label: "Doe", value: "Doe" },
+    ]);
+  });
+});
+
+describe("is filter type tests", () => {
+  const dateFilter: ItemFilter = {
+    start: new Date(),
+    end: new Date(),
+    id: "date",
+    label: "Date",
+  };
+  const searchFilter: ItemFilter = {
+    search: ["test"],
+    id: "search",
+    label: "Search",
+    type: "all",
+  };
+  const keywordFilter: ItemFilter = {
+    currentKeywords: [{ value: "test", label: "test" }],
+    id: "keyword",
+    label: "Keyword",
+    type: "any",
+    allKeywords: [{ value: "test", label: "test" }],
+  };
+
+  describe("isDateFilter", () => {
+    it("should return true for DateFilter", () => {
+      expect(isDateFilter(dateFilter)).toBe(true);
+    });
+
+    it("should return false for non-DateFilter", () => {
+      expect(isDateFilter(searchFilter)).toBe(false);
+      expect(isDateFilter(keywordFilter)).toBe(false);
+    });
+  });
+
+  describe("isSearchFilter", () => {
+    it("should return true for SearchFilter", () => {
+      expect(isSearchFilter(searchFilter)).toBe(true);
+    });
+
+    it("should return false for non-SearchFilter", () => {
+      expect(isSearchFilter(dateFilter)).toBe(false);
+      expect(isSearchFilter(keywordFilter)).toBe(false);
+    });
+  });
+
+  describe("isKeywordFilter", () => {
+    it("should return true for KeywordFilter", () => {
+      expect(isKeywordFilter(keywordFilter)).toBe(true);
+    });
+
+    it("should return false for non-KeywordFilter", () => {
+      expect(isKeywordFilter(dateFilter)).toBe(false);
+      expect(isKeywordFilter(searchFilter)).toBe(false);
+    });
+  });
 });
